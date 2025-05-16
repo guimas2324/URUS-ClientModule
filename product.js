e820c6cf44ba2eac45c356216a4406bb01fbc960
guimas2324 <guifw23@gmail.com>
2025-05-16 14:47:03 -0700

    Create product.js

---
+++ product.js
@@ -0,0 +1,15 @@
+// Basic structure for product management
+
+function addProduct(product) {
+    // Code to add product
+}
+
+function editProduct(productId, updatedProduct) {
+    // Code to edit product
+}
+
+function deleteProduct(productId) {
+    // Code to delete product
+}
+
+module.exports = { addProduct, editProduct, deleteProduct };
